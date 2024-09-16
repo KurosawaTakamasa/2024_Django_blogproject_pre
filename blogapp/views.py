8882b0c4051ddb8e6fb8e210c7d5fd54bff60110
Kurosawa Takamasa <ta.kurosawa.1990@gmail.com>
2024-09-16 09:01:14 +0900

    詳細ページ1

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #django.views.generic.baseからTemplateViewをインポート
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 #モデルBlogPostをインポート
 from .models import BlogPost 
 
@@ -11,3 +11,10 @@ class IndexView(ListView):
     #BlogPostのオブジェクト(レコードの一覧)にorder_by()関数を適用して
     #BlogPostのレコードを登校日順の降順(大きい順=新しい順)に並べる
     queryset = BlogPost.objects.order_by('-posted_at')
+
+class BlogDetail(DetailView):
+    #post.htmlをレンダリング(描画)する
+    template_name = 'post.html'
+    
+    #参照するモデル(データベース)を指定
+    model = BlogPost
